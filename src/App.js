27840255ad171bb956c6acc92c5084c7c5a18bee
RaviPratihast <ravipratihast@gmail.com>
2023-05-17 12:30:56 +0530

    Refactor: remove code from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useNavigate, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import {
   Home,
   Explore,
