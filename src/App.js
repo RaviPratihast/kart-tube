5c0c07d19e40c59ee67b8f004eda3b7aac7e62b2
RaviPratihast <ravipratihast@gmail.com>
2023-05-17 14:42:51 +0530

    style: Update styling for nav.

--- src/App.js
+++ src/App.js
@@ -14,30 +14,41 @@ import "./index.css";
 function App() {
   function getActiveStyle({ isActive }) {
     return {
-      color: isActive ? "blue" : "black",
+      color: isActive ? "white" : "black",
     };
   }
   return (
     <div className="App">
-      <nav className="border-2 bg-blue-600">
-        <NavLink style={getActiveStyle} to="/">
-          Home
-        </NavLink>
-        <NavLink style={getActiveStyle} to="/explore">
-          Explore
-        </NavLink>
-        <NavLink style={getActiveStyle} to="/playlist">
-          Playlist
-        </NavLink>
-        <NavLink style={getActiveStyle} to="/watch-later">
-          Watch Later
-        </NavLink>
-        <NavLink style={getActiveStyle} to="/Liked">
-          Liked
-        </NavLink>
-        <NavLink style={getActiveStyle} to="/History">
-          History
-        </NavLink>
+      <nav className="border-0 bg-blue-600 flex w-full h-14 justify-between items-center">
+        <div className="w-2/3 ml-5 space-x-4">
+          <NavLink style={getActiveStyle} to="/">
+            KartTube
+          </NavLink>
+          <NavLink style={getActiveStyle} to="/explore">
+            Explore
+          </NavLink>
+          <NavLink style={getActiveStyle} to="/playlist">
+            Playlist
+          </NavLink>
+          <NavLink style={getActiveStyle} to="/watch-later">
+            Watch Later
+          </NavLink>
+          <NavLink style={getActiveStyle} to="/Liked">
+            Liked
+          </NavLink>
+          <NavLink style={getActiveStyle} to="/History">
+            History
+          </NavLink>
+        </div>
+        <div className="flex  w-1/2 justify-between ">
+          <div className="w-5/6 space-x-2">
+            <input type="text" placeholder="Search.." />
+            <button type="submit">Search</button>
+          </div>
+          <div className="w-1/6">
+            <button>Login</button>
+          </div>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
