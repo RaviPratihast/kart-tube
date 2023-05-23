0a4340274b4a6298dbe06144dd04bab1e5ef3392
RaviPratihast <ravipratihast@gmail.com>
2023-05-23 13:49:30 +0530

    refactor: updated the export link.

--- src/pages/index-pages.js
+++ src/pages/index-pages.js
@@ -4,5 +4,6 @@ import Playlist from "./playlist-page/playlist";
 import WatchLater from "./watch-later-page/watchLater";
 import Liked from "./liked-page/liked";
 import History from "./history-page/history";
+import VideoDetails from "./videoDetails-page/videoDetails";
 
-export { Home, Explore, Playlist, WatchLater, Liked, History };
+export { Home, Explore,VideoDetails, Playlist, WatchLater, Liked, History };
