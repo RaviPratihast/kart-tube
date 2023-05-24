a5fb9ac665acd83f26b6b03a4686cae7e5580ba8
RaviPratihast <ravipratihast@gmail.com>
2023-05-24 18:34:50 +0530

    docs: changes in documentation shorted name.

--- src/userData/userData.js
+++ src/userData/userData.js
@@ -2,20 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 const videos = [
   {
     id: uuidv4(),
-    title: "Destiny 2: Lightfall - Season of the Deep Launch Trailer",
+    title: "Destiny 2: Lightfall Trailer",
     thumbnail: "https://i.ytimg.com/vi/xZPrldPWXJY/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=xZPrldPWXJY",
   },
   {
     id: uuidv4(),
-    title: "American Idol Winner Iam Tongi",
+    title: "American Idol Winner",
     thumbnail: "https://i.ytimg.com/vi/qaJcYz0BKm4/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=qaJcYz0BKm4",
   },
   {
     id: uuidv4(),
     title:
-      'Iam Tongi & James Blunt: Super Emotional Duet of "Monsters" Makes Idol History - American Idol 2023',
+      'Iam Tongi & James Blunt',
     thumbnail: "https://i.ytimg.com/vi/M9hdHDXyo-8/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=M9hdHDXyo-8",
   },
@@ -27,13 +27,13 @@ const videos = [
   },
   {
     id: uuidv4(),
-    title: "We made a REAL HOLOGRAM Desk like Tony Stark's!",
+    title: "We made a REAL HOLOGRAM Desk",
     thumbnail: "https://i.ytimg.com/vi/PI6VA8ZNL-0/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=PI6VA8ZNL-0",
   },
   {
     id: uuidv4(),
-    title: "Resumen y goles | América (2)1-3(3) Chivas | CL2023 - Semis | TUDN",
+    title: "Resumen y goles |",
     thumbnail: "https://i.ytimg.com/vi/1D_iOST8060/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=1D_iOST8060",
   },
@@ -57,7 +57,7 @@ const videos = [
   },
   {
     id: uuidv4(),
-    title: "Total War: PHARAOH - Announce Trailer",
+    title: "Total War: PHARAOH ",
     thumbnail: "https://i.ytimg.com/vi/lLlD650ZBFQ/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=lLlD650ZBFQ",
   },
@@ -81,44 +81,44 @@ const videos = [
   },
   {
     id: uuidv4(),
-    title: "ENHYPEN (엔하이픈) 'Bite Me' Official MV",
+    title: "ENHYPEN (엔하이픈) 'Bite Me' ",
     thumbnail: "https://i.ytimg.com/vi/wXFLzODIdUI/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=wXFLzODIdUI",
   },
   {
     id: uuidv4(),
-    title: "Web Slingers Miles Morales and Spider-Man 2099 Swing into Fortnite",
+    title: "Web Slingers Miles Morales",
     thumbnail: "https://i.ytimg.com/vi/m3SrWHTJTFw/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=m3SrWHTJTFw",
   },
   {
     id: uuidv4(),
-    title: "The Return to Bloody Nights: Part 1",
+    title: "The Return to Bloody Nights",
     thumbnail: "https://i.ytimg.com/vi/3HGvOnEY_A0/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=3HGvOnEY_A0",
   },
   {
     id: uuidv4(),
-    title: "10 Things to buy at Dollar Tree in May 2023 | DOLLAR TREE FINDS",
+    title: "10 Things to buy ",
     thumbnail: "https://i.ytimg.com/vi/Y5sN2U9M-F4/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=Y5sN2U9M-F4",
   },
   {
     id: uuidv4(),
-    title: "#1 NUGGETS at #7 LAKERS | FULL GAME 4 HIGHLIGHTS | May 22, 2023",
+    title: " FULL GAME 4 HIGHLIGHTS",
     thumbnail: "https://i.ytimg.com/vi/hZo_G9-nob8/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=hZo_G9-nob8",
   },
   {
     id: uuidv4(),
-    title: "Ribwich from The Simpsons | Botched by Babish",
+    title: "Ribwich from The Simpsons",
     thumbnail: "https://i.ytimg.com/vi/fYIk4ohZkYc/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=fYIk4ohZkYc",
   },
   {
     id: uuidv4(),
     title:
-      "Stephen A. & JJ Redick debate the BIGGEST CULPRIT for the Lakers demise 🤯 | First Take",
+      "Stephen A. & JJ Redick ",
     thumbnail: "https://i.ytimg.com/vi/aPnNGTDBsMs/hqdefault.jpg",
     src: "https://www.youtube.com/watch?v=aPnNGTDBsMs",
   },
