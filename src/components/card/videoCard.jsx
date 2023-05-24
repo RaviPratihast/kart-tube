bc9090c835daa727de271c9b5b38ed210235dbb1
RaviPratihast <ravipratihast@gmail.com>
2023-05-24 23:33:49 +0530

    chore: changes in documentation.

--- src/components/card/videoCard.jsx
+++ src/components/card/videoCard.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 function VideoCard({ video }) {
   return (
-    <div className=" h-80 w-64 flex flex-col">
-      <div className="border flex-grow">
+    <div className="flex flex-col rounded overflow-hidden shadow-lg">
+      <div className="relative">
         <img
           src={video.thumbnail}
-          alt="random"
-          className="w-full h-full object-cover"
+          alt="Video Thumbnail"
+          className="w-full h-auto"
         />
       </div>
-      <div className=" flex-shrink">
-
-        <h4>{video.title}</h4>
-        {/* Additional content */}
+      <div className="flex  justify-between bg-white p-4">
+        <div className="mb-2">
+          <h2 className="text-lg font-bold">{video.title}</h2>
+        </div>
+        <div>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Watch Later
+          </button>
+        </div>
       </div>
     </div>
   );
