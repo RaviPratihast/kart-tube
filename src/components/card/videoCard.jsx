d934b21e88013b28fd913b248726e1aaf7c81fb5
RaviPratihast <ravipratihast@gmail.com>
2023-05-25 11:50:22 +0530

    style: added some style to button.

--- src/components/card/videoCard.jsx
+++ src/components/card/videoCard.jsx
@@ -15,7 +15,21 @@ function VideoCard({ video }) {
           <h2 className="text-lg font-bold">{video.title}</h2>
         </div>
         <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button className=" flex  gap-1 bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
             Watch Later
           </button>
         </div>
--- src/pages/videoDetails-page/videoDetails.jsx
+++ src/pages/videoDetails-page/videoDetails.jsx
@@ -17,12 +17,12 @@ function VideoDetails() {
               </div>
               <div className="flex  flex-col  text-slate">
                 <div className="flex flex-col justify-between  between h-50 w-full">
-                  <div className="flex  h-10 w-full">
+                  <div className="flex  h-10 w-full mt-2">
                     <div className="flex justify-start items-center p-2 w-1/2">
                       <h2 className="">{video.title}</h2>
                     </div>
                     <div className="flex justify-between items-center   w-1/2 p-2">
-                      <button className="flex gap-2">
+                      <button className="flex gap-2  bg-blue-500 border hover:border-blue-500 hover:bg-white text-white hover:text-blue-700 py-2 px-4 rounded">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -39,7 +39,7 @@ function VideoDetails() {
                         </svg>
                         Like
                       </button>
-                      <button className="flex gap-2">
+                      <button className="flex gap-2  bg-blue-500 border hover:border-blue-500 hover:bg-white text-white hover:text-blue-700 py-2 px-4 rounded">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -56,7 +56,7 @@ function VideoDetails() {
                         </svg>
                         Watch Later
                       </button>
-                      <button className="flex gap-2">
+                      <button className="flex gap-2  bg-blue-500 border hover:border-blue-500 hover:bg-white text-white hover:text-blue-700 py-2 px-4 rounded">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
@@ -73,7 +73,7 @@ function VideoDetails() {
                         </svg>
                         Share
                       </button>
-                      <button className="flex gap-2">
+                      <button className="flex gap-2  bg-blue-500 border hover:border-blue-500 hover:bg-white text-white hover:text-blue-700 py-2 px-4 rounded">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
