3d2c9fa1fa48cc390cfbc76e667ae9f0a34a6867
RaviPratihast <ravipratihast@gmail.com>
2023-05-25 21:42:22 +0530

    refactor: removed button.

--- src/components/card/videoCard.jsx
+++ src/components/card/videoCard.jsx
@@ -20,23 +20,6 @@ function VideoCard({ video }) {
           <h2 className="text-sm">{video.title}</h2>
         </div>
         <div>
-          {/* <Button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-         
-          </Button> */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
