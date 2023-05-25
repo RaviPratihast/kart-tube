e1b060bf260ba390d371966e47b069f9c6e06f36
RaviPratihast <ravipratihast@gmail.com>
2023-05-25 11:36:15 +0530

    refactor: just tested.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { BrowserRouter as Router } from "react-router-dom";
 // import "./tailwind.config";
 import "./index.css";
