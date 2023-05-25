07e44a9b363046c6e8c2ea5ddba743f05ba58599
RaviPratihast <ravipratihast@gmail.com>
2023-05-25 12:25:31 +0530

    feat: onClick prop passed and destructured.

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Button({children}) {
+function Button({children,onClick}) {
   return (
-    <button className=" flex  gap-1 bg-blue-500 border hover:border-blue-500 hover:bg-white text-white hover:text-blue-700 py-2 px-4 rounded">{children}</button>
+    <button onClick={onClick} className=" flex  gap-1 bg-blue-500 border hover:border-blue-500 hover:bg-white text-white hover:text-blue-700 py-2 px-4 rounded">{children}</button>
   );
 }
 
