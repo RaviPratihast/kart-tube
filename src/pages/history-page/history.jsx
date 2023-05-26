b94bad0024f9a03b24bba64e47fed5ad5a542c4e
RaviPratihast <ravipratihast@gmail.com>
2023-05-26 12:36:30 +0530

    style: default page is created.

--- src/pages/history-page/history.jsx
+++ src/pages/history-page/history.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Button from '../../components/button/button'
 
 function History() {
   return (
-    <div>History</div>
+    <div className="flex h-screen flex-col">
+    <div className=" flex flex-col  mt-28 w-full h-full ">
+      <div className="  h-20 w-60 flex flex-col justify-center items-center ml-10">
+        {/* count of playlist */}
+        <h3 className='text-xl'>Watched History</h3>
+      </div>
+      <div className="m-20 flex justify-center items-center">
+        {/* playlists */}
+        <div className="shadow-lg h-60 w-auto p-20 flex flex-col justify-center items-center gap-2 rounded-md  border border-blue-500 ml-10">
+          <h2 className="flex flex-col text-lg">Looks like you haven't started watching.</h2>
+          <Button>Watch Now</Button>
+        </div>
+
+        {/* div with same above style, inside this div there will be added playlist, first the playlist will be created. */}
+      </div>
+    </div>
+  </div>
   )
 }
 
