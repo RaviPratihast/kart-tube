c8b22c4fcca89fd529bb4db2698ad2269f4ccba2
RaviPratihast <ravipratihast@gmail.com>
2023-05-26 21:26:57 +0530

    style: home ui is done.

--- src/pages/home-page/home.jsx
+++ src/pages/home-page/home.jsx
@@ -1,30 +1,32 @@
 import "tailwindcss/tailwind.css";
 import React from "react";
+import Button from "../../components/button/button";
 // import imageSrc from "/public/images/comic.jpg";
 import "../../index.css";
 
 function Home() {
   return (
-    <div className="flex flex-col  flex-wrap shadow-lg text-white bg-slate-950 border h-auto w-18">
-      <div className=" flex justify-between items-center mx-auto my-6 ">
-        {/* title and image */}
-        <div className="flex flex-col justify-center items-center  w-1/2 h-full gap-4"></div>
-
-        <div className="w-1/2">
-          <h1 className="text-4xl">kartTube</h1>
-          <button className="border h-8 w-32 hover:text-blue-600 bg-blue-600 hover:bg-white rounded-md">
-            Explore More
-          </button>
+    <div className="flex flex-col justify-center items-center">
+      <div className="   h-auto w-1/2 p-2 mt-48 flex gap-10 justify-center items-center  rounded-md  mx-10">
+        <div className=" p-3 w-3/2 h-full flex flex-col justify-center items-center">
+          <div className=" w-3/2  p-4 gap-2">
+            <h1 className="flex flex-col text-3xl font-bold ">kartTube</h1>
+            <p className="mt-2 text-justify">
+              "Explore, Discover, and Unwind with kartTube - Your Ultimate Video
+              Library"
+            </p>
+          </div>
+          <div className=" w-full h-auto flex justify-end p-2">
+            <Button>Watch Now</Button>
+          </div>
+        </div>
+        <div className="">
           <img
-            className="h-full h-full object-cover"
-            src="/public/images/comic.jpg"
-            alt="galaxy"
+            src="/image/undraw_video_streaming_re_v3qg.svg"
+            alt="media player"
+            className="w-full h-full"
           />
         </div>
-      </div>
-      <div className=" mx-auto my-6 border">
-        {/* three divs that will have png or gif, which will played when hovered */}
-        <div>222</div>
       </div>
     </div>
   );
