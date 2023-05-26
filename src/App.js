12d7e1b479216a53066190db0753d80deaa2e3f0
RaviPratihast <ravipratihast@gmail.com>
2023-05-27 01:57:11 +0530

    style: changes with width and font.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   History,
   Login,
 } from "./pages/index-pages";
+import Button from "./components/button/button";
 // import {Header,Navbar} from "./components/index-components"
 import "./index.css";
 
@@ -21,11 +22,13 @@ function App() {
   }
   return (
     <div className="App">
-      <nav className="border-0 bg-blue-500 flex w-full h-20 justify-between items-center fixed">
-        <div className="w-2/3 ml-5 space-x-4">
-          <NavLink style={getActiveStyle} to="/">
-            KartTube
-          </NavLink>
+      <nav className=" bg-blue-500 flex w-screen h-20 justify-between gap-2 items-center fixed inset-0">
+        <div className="w-1/2 ml-5 p-2 flex items-center gap-4 text-lg">
+          <div className="font-bold text-xl">
+            <NavLink style={getActiveStyle} to="/">
+              KartTube
+            </NavLink>
+          </div>
           <NavLink style={getActiveStyle} to="/explore">
             Explore
           </NavLink>
@@ -42,13 +45,34 @@ function App() {
             History
           </NavLink>
         </div>
-        <div className="flex  w-1/2 justify-between ">
-          <div className="w-5/6 space-x-2">
+        <div className="flex w-1/2 justify-between mr-3">
+          {/* <div className="w-5/6 space-x-2">
             <input type="text" placeholder="Search.." />
             <button type="submit">Search</button>
+          </div> */}
+          <div className="w-1/2 flex justify-center items-center p-2 bg-white rounded">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full pl-2 pr-2 outline-none"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
           </div>
           <div className="w-1/6">
-            <button>Login</button>
+            <Button>Login</Button>
           </div>
         </div>
       </nav>
