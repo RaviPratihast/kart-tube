ce64849161995ebbd680f8b33d6a02fb9b1d46e9
RaviPratihast <ravipratihast@gmail.com>
2023-05-27 10:36:26 +0530

    feat: navigation implemented.

--- src/pages/watch-later-page/watchLater.jsx
+++ src/pages/watch-later-page/watchLater.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../components/button/button";
 
 function WatchLater() {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate("/explore");
+  }
   return (
     <div className="flex h-screen flex-col">
       <div className=" flex flex-col  mt-28 w-full h-full ">
@@ -13,7 +18,7 @@ function WatchLater() {
           {/* playlists */}
           <div className="shadow-lg h-60 w-auto p-20 flex flex-col justify-center items-center gap-2 rounded-md  border border-blue-500 ml-10">
             <h2 className="flex flex-col text-lg">Looks like you haven't added anything in Watch Later yet.</h2>
-            <Button>Watch Now</Button>
+            <Button onClick={handleClick}>Watch Now</Button>
           </div>
 
           {/* div with same above style, inside this div there will be added playlist, first the playlist will be created. */}
