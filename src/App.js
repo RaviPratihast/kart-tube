a60782f33b08fa9e8cb6f2a41dd40ffaefd83eeb
RaviPratihast <ravipratihast@gmail.com>
2023-05-27 11:13:17 +0530

    fix: updated svg attribute as JSX format.

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
       color: isActive ? "white" : "black",
     };
   }
-  
-  function handleClick(){
-    navigate("/login")
+
+  function handleClick() {
+    navigate("/login");
   }
   return (
     <div className="App">
@@ -52,10 +52,6 @@ function App() {
           </NavLink>
         </div>
         <div className="flex w-1/2 justify-between mr-3">
-          {/* <div className="w-5/6 space-x-2">
-            <input type="text" placeholder="Search.." />
-            <button type="submit">Search</button>
-          </div> */}
           <div className="w-1/2 flex justify-center items-center p-2 bg-white rounded">
             <input
               type="text"
@@ -66,13 +62,13 @@ function App() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
-              class="w-5 h-5"
+              className="w-5 h-5"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
               />
             </svg>
--- src/components/card/videoCard.jsx
+++ src/components/card/videoCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../button/button";
+// import Button from "../button/button";
 
 function VideoCard({ video }) {
   return (
@@ -24,14 +24,14 @@ function VideoCard({ video }) {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-6 h-6 text-blue-500 cursor-pointer"
             onClick={() => console.log("clicked")}
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
             />
           </svg>
