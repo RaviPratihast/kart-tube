0fd27ad13ed35205972c28e0cd1ba052b5231cd6
RaviPratihast <ravipratihast@gmail.com>
2023-05-27 13:16:19 +0530

    feat: passed children as prop.

--- src/components/card/videoCard.jsx
+++ src/components/card/videoCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 // import Button from "../button/button";
 
-function VideoCard({ video }) {
+function VideoCard({ video, children }) {
   return (
     <div className="flex  flex-col w-56 h-56  rounded overflow-hidden shadow-lg">
       <Link to={`/video/${video.id}`}>
@@ -19,23 +19,7 @@ function VideoCard({ video }) {
         <div className="mb-2">
           <h2 className="text-sm">{video.title}</h2>
         </div>
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-blue-500 cursor-pointer"
-            onClick={() => console.log("clicked")}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </div>
+        <div>{children}</div>
       </div>
     </div>
   );
--- src/pages/explore-page/explore.jsx
+++ src/pages/explore-page/explore.jsx
@@ -12,7 +12,23 @@ function Explore() {
     <div className="flex flex-wrap items-center ">
       <div className="flex flex-wrap justify-start mx-10 mt-28 mb-5 gap-1  border-slate-800 ">
         {state.initialVideo.map((video) => {
-          return <VideoCard key={video.id} video={video} />;
+          return <VideoCard key={video.id} video={video}>
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 text-blue-500 cursor-pointer"
+            onClick={() => console.log("clicked")}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          </VideoCard>;
         })}
       </div>
     </div>
