05063e21afab75497fa51a4239330f936c0530b1
RaviPratihast <ravipratihast@gmail.com>
2023-05-28 14:21:16 +0530

    refactor: remove some unwanted codes.

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,21 +1,14 @@
 function reducer(state, action) {
   switch (action.type) {
     case "ADD_TO_LIKED":
-      // Perform logic to add the video to the liked object
-      console.log("Adding to liked:", action.payload);
-      // return {
-      //   ...state, // Spread the current state
-      //   liked: [...state.liked, action.payload], // Add the video to the liked array
-      // };
-
       if (
         state.liked.some((likedVideo) => likedVideo.id === action.payload.id)
       ) {
         return state;
       } else {
         return {
-          ...state, // Spread the current state
-          liked: [...state.liked, action.payload], // Add the video to the liked array
+          ...state,
+          liked: [...state.liked, action.payload],
         };
       }
     case "REMOVE_FROM_LIKED":
@@ -26,7 +19,6 @@ function reducer(state, action) {
         ),
       };
 
-    // Handle other action types here
     default:
       return state; // Return the current state for unrecognized actions
   }
