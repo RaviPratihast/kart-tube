ea6d4ef087a69c57ebff85093e4f2fd9959671d1
RaviPratihast <ravipratihast@gmail.com>
2023-06-07 20:57:07 +0530

    test: set up useReducer for authContext.

--- src/context/auth-context/auth-context.js
+++ src/context/auth-context/auth-context.js
@@ -1,12 +1,27 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer, useState } from "react";
+import authenticationReducer from "../../reducers/authenticationReducer";
 
 const AuthContext = createContext();
 const useAuth = () => useContext(AuthContext);
 
+const initialState = {
+  users: [
+    { user: "Ravi", password: "123" },
+    { user: "a", password: "1" },
+  ],
+};
+
 const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
+  //   i will also merge this above usestate in the reducer, cause i am already using reducer then what is the point of using it?
+  const [stateAuth, dispatchAuth] = useReducer(
+    authenticationReducer,
+    initialState
+  );
   return (
-    <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+    <AuthContext.Provider
+      value={{ stateAuth, dispatchAuth, loggedIn, setLoggedIn }}
+    >
       {children}
     </AuthContext.Provider>
   );
